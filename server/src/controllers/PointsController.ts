a256c241bbcd3171ace668a2c907c3d628836e25
GabriPires <gabriel.santos.pes@gmail.com>
2020-06-02 17:45:45 -0300

    Imagem estática no cadastro

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -52,7 +52,7 @@ class PointsController {
     const trx = await knex.transaction();
 
     const point = {
-      image: 'image-fake',
+      image: 'https://images.unsplash.com/photo-1506484381205-f7945653044d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
       name,
       email,
       whatsapp,
