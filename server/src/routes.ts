a2e5ff2f4e83566bf0f693b90173935d2cb5a7d2
GabriPires <gabriel.santos.pes@gmail.com>
2020-06-02 17:05:19 -0300

    Cadastro no point_items

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -29,7 +29,9 @@ routes.post('/points', async (request, response) => {
     items
   } = request.body;
 
-  await knex('points').insert({
+  const trx = await knex.transaction();
+
+  const insertedIds = await trx('points').insert({
     image: 'image-fake',
     name,
     email,
@@ -40,7 +42,18 @@ routes.post('/points', async (request, response) => {
     uf,
   });
 
-  return response.json({success: true})
+  const point_id = insertedIds[0];
+
+  const pointItems = items.map((item_id: number) => {
+    return {
+      item_id,
+      point_id
+    };
+  })
+
+  await trx('point_items').insert(pointItems);
+
+  return response.json({ success: true })
 })
 
 export default routes;
