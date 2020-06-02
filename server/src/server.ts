d0670ccedd6e93c110df7fcc2bafbec738a8e9d3
GabriPires <gabriel.santos.pes@gmail.com>
2020-06-02 10:57:56 -0300

    Utilizando rotas e params

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,39 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json());
+
+const users = [
+  'Diego',
+  'Cleiton',
+  'Robson'
+];
 
 app.get('/users', (request, response) => {
-  console.log('Listagem de usuários');
+  const search = String(request.query.search);
 
-  response.json([
-    'Diego',
-    'Cleiton',
-    'Robson'
-  ]);
+  const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
+
+  return response.json(filteredUsers);
+})
+
+app.get('/users/:id', (request, response) => {
+  const id = Number(request.params.id);
+
+  const user = users[id];
+
+  return response.json(user)
+})
+
+app.post('/users', (request, response) => {
+  const data = request.body;
+
+  const user = {
+    name: data.name,
+    email: data.email
+  };
+
+  return response.json(user);
 })
 
 app.listen(3333);
