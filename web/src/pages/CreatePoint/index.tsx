1c08aa29b52ce6c3934d13141937a1fc5a6c8aa7
GabriPires <gabriel.santos.pes@gmail.com>
2020-06-03 14:19:30 -0300

    Cadastrando dados do ponto de coleta no banco

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState, ChangeEvent } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState, ChangeEvent, FormEvent } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi'
 import { Map, TileLayer, Marker } from 'react-leaflet';
 import axios from 'axios';
@@ -40,7 +40,9 @@ const CreatePoint = () => {
   const[selectedUf, setSelectedUf] = useState('0');
   const[selectedCity, setSelectedCity] = useState('0');
   const[selectedItems, setSelectedItems] = useState<number[]>([]);
-  const[selectedPoisition, setSelectedPosition] = useState<[number, number]>([0, 0]);
+  const[selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
+
+  const history = useHistory();
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(position => {
@@ -95,7 +97,31 @@ const CreatePoint = () => {
     setFormData({ ...formData, [name]: value });
   }
 
-  
+  async function handleSubmit (event: FormEvent) {
+    event.preventDefault();
+
+    const { name, email, whatsapp } = formData;
+    const uf = selectedUf;
+    const city = selectedCity;
+    const [latitude, longitude] = selectedPosition;
+    const items = selectedItems;
+
+    const data = {
+      name,
+      email,
+      whatsapp,
+      uf,
+      city,
+      latitude,
+      longitude,
+      items
+    }
+
+    // console.log(data);
+    await api.post('points', data);
+    alert('Ponto de coleta criado!');
+    history.push('/');
+  }
 
   function handleSelectItem(id: number) {
     const alreadySelected = selectedItems.findIndex(item => item === id);
@@ -118,7 +144,7 @@ const CreatePoint = () => {
         </Link>
       </header>
 
-      <form>
+      <form onSubmit={handleSubmit}>
         <h1>Cadastro do <br/> ponto de coleta</h1>
         
         <fieldset>
@@ -169,7 +195,7 @@ const CreatePoint = () => {
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
 
-            <Marker position={selectedPoisition}/>
+            <Marker position={selectedPosition}/>
           </Map>
 
           <div className="field-group">
