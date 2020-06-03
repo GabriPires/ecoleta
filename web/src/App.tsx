eaf8eca334a26746b8cd7c5ddbb8687a4181da6b
GabriPires <gabriel.santos.pes@gmail.com>
2020-06-03 11:41:21 -0300

    Criação cadastro de ponto de coleta

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import './App.css';
 
-import Header from './Header';
-import Home from './pages/Home';
+import Routes from './routes';
 
 function App() {
   return (
     <div>
-      <Home />
+      <Routes />
     </div>
   );
 }
